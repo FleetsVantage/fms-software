3ae55793d1248d86a94ed915be47236d363c4493
Ajin Sunny <ajin.sunny@gmail.com>
2023-06-27 11:14:32 -0400

    forgot semicolon 🙂

--- pages/index.js
+++ pages/index.js
@@ -25,7 +25,7 @@ export default function Home() {
       const data = await res.json();
 
       if (res.ok) {
-        alert(data.message)
+        alert(data.message);
       } else {
         alert(data.message);
       }
