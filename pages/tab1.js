871115caf41cf37e860eb8c3ac2256068f5c6a1d
Ajin Sunny <ajin.sunny@gmail.com>
2024-01-18 22:05:58 -0600

    Add dynamic tab functionality

--- pages/tab1.js
+++ pages/tab1.js
@@ -1,7 +1,10 @@
 import Head from 'next/head'
 import styles from '../styles/Tab.module.css'
+import { useState } from 'react'
 
 export default function Tab1() {
+  const [activeTab, setActiveTab] = useState(1);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -11,12 +14,9 @@ export default function Tab1() {
       </Head>
 
       <div className={styles.tabs}>
-        <input type="radio" id="radio-1" name="tabs" checked />
-        <label className={styles.tab} htmlFor="radio-1">Upcoming<span className={styles.notification}>2</span></label>
-        <input type="radio" id="radio-2" name="tabs" />
-        <label className={styles.tab} htmlFor="radio-2">Development</label>
-        <input type="radio" id="radio-3" name="tabs" />
-        <label className={styles.tab} htmlFor="radio-3">Completed</label>
+        <div className={`${styles.tab} ${activeTab === 1 && styles.active}`} onClick={() => setActiveTab(1)}>Upcoming<span className={styles.notification}>2</span></div>
+        <div className={`${styles.tab} ${activeTab === 2 && styles.active}`} onClick={() => setActiveTab(2)}>Development</div>
+        <div className={`${styles.tab} ${activeTab === 3 && styles.active}`} onClick={() => setActiveTab(3)}>Completed</div>
         <span className={styles.glider}></span>
       </div>
     </div>
